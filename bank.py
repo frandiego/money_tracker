3a1f7978a7a8ab49ccc72a69dc0257f1e4111f0d
Fran Diego Acosta <franciscojosediegoacosta@gmail.com>
2021-07-26 16:59:47 +0200

    bank integration

---
+++ bank.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from itertools import chain
+
+
+class Bank:
+
+    url = {
+        'requisition': 'https://ob.nordigen.com/api/requisitions/{}/',
+        'transactions': 'https://ob.nordigen.com/api/accounts/{}/transactions/'
+    }
+
+    @classmethod
+    def headers(self):
+        return {'accept': 'application/json', 'Authorization': f"Token {os.getenv('TOKEN')}"}
+
+    @classmethod
+    def get(cls, url):
+        return requests.get(url, headers=cls.headers())
+
+    @classmethod
+    def _requisition(cls):
+        return cls.get(cls.url.get('requisition').format(os.getenv('REQUISITION')))
+
+    @classmethod
+    def accounts(cls):
+        return cls._requisition().json().get('accounts', [])
+
+
+    @classmethod
+    def _transactions(cls):
+        return {i:cls.get(cls.url.get('transactions').format(i)).json().get('transactions', {}) for i in cls.accounts()}
+
+    @classmethod
+    def _merge(cls,transaction:dict,  transaction_type:str, account_id:id):
+        return transaction | {'transaction_type':transaction_type, 'account_id': account_id}
+
+    @classmethod
+    def nested_transactions(cls):
+        return [[[cls._merge(i, t, k) for i in j] for t, j in v.items()] for k, v in cls._transactions().items()]
+
+    @classmethod
+    def flat_transactions(cls):
+        return list(chain(*chain(*cls.nested_transactions())))
+
+
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+import pandas as pd
 import streamlit as st
 
-st.text('Hello World')
+from bank import Bank
+
+os.environ.update({i[0]: i[1] for i in [i.split('=') for i in open('.env').read().split()]})
+
+st.write(pd.DataFrame(Bank.flat_transactions()))
