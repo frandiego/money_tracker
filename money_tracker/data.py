90c580edab1306ff5a1cc5b6ab77a63797b0abd6
Fran Diego Acosta <franciscojosediegoacosta@gmail.com>
2021-08-05 14:18:45 +0200

    hotfix in Data

--- money_tracker/data.py
+++ money_tracker/data.py
@@ -11,28 +11,32 @@ class Data:
         for i in set(columns).difference(set(df.columns)):
             df[i] = fill
         return df[columns]
+    
+    @classmethod
+    def tidy_transactions(cls, df:pd.DataFrame()):
+        df = cls.coerce_columns(df=df, columns=C.transaction_keys, fill='')
+        return df.drop_duplicates().sort_values(C.valueDate, ascending=False).reset_index(drop=True)
 
+    @classmethod
+    def read_bank_transactions(cls):
+        request = Bank.transactions()
+        safe =  request if request else []
+        return cls.tidy_transactions(pd.DataFrame.from_records(safe))
 
     @classmethod
     def read_saved_transactions(cls) -> pd.DataFrame:
         if os.path.exists(C.filename_transactions):
-            return cls.coerce_columns(pd.read_feather(C.filename_transactions), C.transaction_keys)
-        return pd.DataFrame()
-
+            return cls.tidy_transactions(pd.read_feather(C.filename_transactions))
+        return cls.tidy_transactions(pd.DataFrame())
+    
     @classmethod
-    def read_bank_transaciton(cls) -> pd.DataFrame:
-        transactions = Bank.transactions()
-        if transactions:
-            return cls.coerce_columns(pd.read_feather(C.filename_transactions), C.transaction_keys)
-        return pd.DataFrame()
+    def read_transactions(cls):
+        df = pd.concat([cls.read_saved_transactions(), cls.read_bank_transactions()])
+        return cls.tidy_transactions(df)
 
     @classmethod
     def transactions(cls) -> pd.DataFrame:
-        df = pd.concat([cls.read_saved_transactions(), cls.read_bank_transaciton()])
-        df = cls.coerce_columns(df, C.transaction_keys)
-        df.drop_duplicates(inplace=True)
-        df.sort_values(C.valueDate, ascending=False, inplace=True)
-        df.reset_index(drop=True, inplace=True)
+        df = cls.read_transactions()
         df.to_feather(C.filename_transactions)
         return df
 
